12cb4db013de1489d88348d1d7372dc55f36ac71
Anton20HD <anton.haglund@iths.se>
2025-06-29 13:35:30 +0200

    Create entity class for user

---
+++ backend/Models/User.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class User
+    {
+        //Int funkar men Guid kan vara bättre för bla säkerhet och exportering av data mellan olika system
+        public int Id { get; set; } // unikt id
+
+        // required förhindrar ofullständiga objekt, kompatibelt med null-säkerhet, slipper skapa konstruktor
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
