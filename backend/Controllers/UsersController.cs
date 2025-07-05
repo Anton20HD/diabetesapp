a1d4e08edd497c9edda95464b34b0e505b3739cc
Anton20HD <anton.haglund@iths.se>
2025-07-05 14:18:23 +0200

    Add getallusers function in controller class and a dbcontext constructor. Add some dummy data to test controller

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers
 {
+    //localhost:xxxx/api/
     [ApiController]
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {   
+             //Returnerar ett 200 meddelande
+            return Ok(dbContext.Users.ToList());
+        }
         
     }
 }
