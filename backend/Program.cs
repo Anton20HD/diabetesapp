25f124f33f6f448639656cb4961beffcb12c689a
Anton20HD <anton.haglund@iths.se>
2025-07-05 13:53:25 +0200

    Successfully connected to sql server and create db with user table. Create userscontroller file

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,13 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+
+// För att injecta vår dbcontext 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
