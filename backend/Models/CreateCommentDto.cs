f52412385a4ce7089e339b68d2552fe0f014c5e8
Anton20HD <anton.haglund@iths.se>
2025-07-10 18:19:51 +0200

    Change name of addusedto to createuserdto to make it more unitary. Create a commentdto

---
+++ backend/Models/CreateCommentDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class CreateCommentDto
+    {
+        public required string Author { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required DateTime PublishedDate { get; set; }
+
+        //Foreign key
+        public int PostId { get; set; }
+
+    }
+}
