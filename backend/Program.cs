604aa77f2cdf8d9981334096a55ee89039bacd31
Anton20HD <anton.haglund20@gmail.com>
2025-07-13 12:01:29 +0200

    Create Authcontroller for handling authentication. Install necessary packages for auth. Define jwt in appsettings and implement configuration in program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using backend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -9,9 +12,26 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWTKey:ValidIssuer"],
+        ValidAudience = builder.Configuration["JWTKey:ValidAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTKey:Secret"]))
+
+    };
+
+    });
+
 var app = builder.Build();
 
 app.MapControllers();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run();
     
