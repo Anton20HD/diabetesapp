6e2620c1cb4e15af0300d494f4102e396aec3f8f
Anton20HD <anton.haglund20@gmail.com>
2025-08-11 11:12:42 +0200

    Add video to startpage

--- frontend/src/app/startPage/page.tsx
+++ frontend/src/app/startPage/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+
+//import Products from "@/app/components/products/page";
+
 
 const StartPage = () => {
-  return (
-    <div>StartPage</div>
-  )
-}
+  // const [products, setProducts] = useState([]);
+  // useEffect(() => {
+  //   fetch("")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setProducts(data);
+  //     });
+  // }, []);
 
-export default StartPage
+
+
+
+
+
+  return (
+    <div className="h-screen bg-repeat bg-cover bg-center absolute overflow-hidden">
+      <div>
+        <video
+          className="w-600 h-150  object-cover"
+          src="/videos/startpage.mp4"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+
+      
+
+      {/* <Products /> */}
+      
+    </div>
+  );
+};
+
+export default StartPage;
